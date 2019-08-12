0e623e7aeae874389cfb1b550c103bbdbc4f0622
Solito Reyes II <solitonyc@gmail.com>
2019-08-12 09:49:33 -0400

    education section styles refinement

--- src/components/Education.js
+++ src/components/Education.js
@@ -9,7 +9,7 @@ export default class Education extends Component {
                 <div className="education_item_a">
                     <p className="school_name">General Assembly</p>
                     <p className="degree_name">Web Development Immersive</p>
-                    <p className="degree_descrip">HTML, CSS, JavaScript, Node.js, React.js, PostGreSQL, Ruby, Ruby on Rails, GitHub</p>
+                    <p className="degree_descrip">Compressed course in HTML, CSS/Flex/Grid, Git, JavaScript, Node.js, React.js, Express, PostGreSQL, Ruby, Ruby on Rails, Heroku, Agile, Khan Ban</p>
                     <p className="location_style">Manhattan, NY</p>
                 </div>
                 <div className="education_item_b">
@@ -23,8 +23,7 @@ export default class Education extends Component {
                     <p className="degree_name">Political Science // Bachelors Degree // GPA: 3.5</p>
                     <p className="location_style">W. Long Branch, NJ</p>
                 </div>
-
-                <div className='education_section_title'>Certifications</div>
+                <div className='education_section_subtitle'>Certifications</div>
                 <div className="education_item_a">
                     <p className="school_name">MCSE</p>
                     <p className="degree_name">Microsoft Certified Systems Engineer</p>
