dfbd9eff319724856f35f1e971a99a2050848e5a
Solito Reyes II <solitonyc@gmail.com>
2019-08-11 23:16:04 -0400

    education section fully implemented, content, style, css, color, background shading, spacing, fonts

--- src/components/Education.js
+++ src/components/Education.js
@@ -5,7 +5,32 @@ export default class Education extends Component {
     render() {
         return (
             <div className='education_section' id='education'>
-                EDUCATION PAGE
+                <div className='education_section_title'>Education</div>
+                <div className="education_item_a">
+                    <p className="school_name">General Assembly</p>
+                    <p className="degree_name">Web Development Immersive</p>
+                    <p className="degree_descrip">HTML, CSS, JavaScript, Node.js, React.js, PostGreSQL, Ruby, Ruby on Rails, GitHub</p>
+                    <p className="location_style">Manhattan, NY</p>
+                </div>
+                <div className="education_item_b">
+                    <p className="school_name">Devry</p>
+                    <p className="degree_name">Telecommunications</p>
+                    <p className="degree_descrip">Telecommunications, Electrical Engineering, Intranetworking, Infrastructure</p>
+                    <p className="location_style">North Brunswick, NJ</p>
+                </div>
+                <div className="education_item_a">
+                    <p className="school_name">Monmouth University</p>
+                    <p className="degree_name">Political Science // Bachelors Degree // GPA: 3.5</p>
+                    <p className="location_style">W. Long Branch, NJ</p>
+                </div>
+
+                <div className='education_section_title'>Certifications</div>
+                <div className="education_item_a">
+                    <p className="school_name">MCSE</p>
+                    <p className="degree_name">Microsoft Certified Systems Engineer</p>
+                    <p className="degree_descrip">Intranetworking, Infrastructure, DevOps, TCP/IP, DNS, Routing, Switching, Firewalls, NOS, Domain Management</p>
+                    <p className="location_style">New York, NY</p>
+                </div>
             </div>
         )
     }
