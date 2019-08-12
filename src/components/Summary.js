6ad73814495fb776b53db80bec7673fed7c4e221
Solito Reyes II <solitonyc@gmail.com>
2019-08-12 16:21:43 -0400

    summay section background image set

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import summaryBgImage from './images/backgrounds/summary-bg.jpg';
 import './summary.css';
 
 export default class Summary extends Component {
     render() {
+        let style = {
+            backgroundImage: `url(${summaryBgImage})`
+        }
         return (
-            <div className='summary_section' id='summary'>
+            <div style={style} className='summary_section' id='summary'>
                 <div className="skills_section_title">SUMMARY</div>
                 <div className="skills_content">Former CTO, with over 18 years of tech industry experience, seeking a role leveraging leadership qualities, managerial aptitude, busines acumen, software skills and experiences working with tech-minded companies in New York. Possesses extensive experiences overseeing the development and production of software products through its extensive lifecycle from board-room idea to production.</div>
                 <div className="skills_section_subtitle">Possible Roles:</div>
