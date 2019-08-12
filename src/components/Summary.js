8471365f8cbff86c3987c40f7ab1929fb42db6fb
Solito Reyes II <solitonyc@gmail.com>
2019-08-12 16:46:45 -0400

    summary page styling, font sizing, and shading done

--- src/components/Summary.js
+++ src/components/Summary.js
@@ -9,11 +9,12 @@ export default class Summary extends Component {
         }
         return (
             <div style={style} className='summary_section' id='summary'>
-                <div className="skills_section_title">SUMMARY</div>
-                <div className="skills_content">Former CTO, with over 18 years of tech industry experience, seeking a role leveraging leadership qualities, managerial aptitude, busines acumen, software skills and experiences working with tech-minded companies in New York. Possesses extensive experiences overseeing the development and production of software products through its extensive lifecycle from board-room idea to production.</div>
-                <div className="skills_section_subtitle">Possible Roles:</div>
-                <div className="skills_content">Seeking role as Chief Technology Officer, Product Manager or Project Manager. Also has hands on full-stack coding experience.</div>
-              
+                <div className="summary_content_container">
+                    <div className="summary_section_title">SUMMARY</div>
+                    <div className="summary_content">Former CTO, with over 18 years of tech industry experience, seeking a role leveraging leadership qualities, managerial aptitude, busines acumen, software skills and experiences working with tech-minded companies in New York. Possesses extensive experiences overseeing the development and production of software products through its extensive lifecycle from board-room idea to production.</div>
+                    <div className="summary_section_subtitle">Possible Roles:</div>
+                    <div className="summary_content">Seeking role as Chief Technology Officer, Product Manager or Project Manager. Also has hands on full-stack coding experience.</div>
+                </div>
             </div>
         )
     }
