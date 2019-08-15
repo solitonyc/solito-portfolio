53b83e5453fdb46f9ffe07fa00bb7e69cfe71141
Solito Reyes II <solitonyc@gmail.com>
2019-08-14 23:40:47 -0400

    portfolio revisions, added another card, revised all card content

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -6,6 +6,7 @@ import portfolioImage02 from './images/portfolio-02.jpg';
 import portfolioImage03 from './images/portfolio-03.jpg';
 import portfolioImage04 from './images/portfolio-04.jpg';
 import portfolioImage05 from './images/portfolio-05.jpg';
+import portfolioImage06 from './images/portfolio-06.jpg';
 
 export default class Portfolio extends Component {
     render() {
@@ -28,7 +29,7 @@ export default class Portfolio extends Component {
                         <div class='portfolio_card'>
                             <h2 className="portfolio_card_title">Full Stack, Social Network</h2>
                             <p className="portfolio_content">A full-stack, social media CRUD application for General Assembly graduates.</p>
-                            <p className="portfolio_content">HTML | CSS Flex | Javascript | Auth | React.js | Express | Rails | PostGreSQL | GitHub | Heroku</p>
+                            <p className="portfolio_content">HTML | CSS Flex | Javascript | Auth | React.js | Express | Rails | PostGreSQL | GitHub | Heroku | Photoshop</p>
                             <a href='http://ensemble-ga.surge.sh/' target="_blank"><img className='portfolio_box_image' alt='' src={portfolioImage02} /></a>      
                             
                         </div>
@@ -36,7 +37,7 @@ export default class Portfolio extends Component {
                         <div class='portfolio_card'>
                             <h2 className="portfolio_card_title">Picture Puzzle Game</h2>        
                             <p className="portfolio_content">A simple image puzzle that utilizes drag-and-drop. Place random pieces from one side of the board to its proper position on the other to complete the picture. I developed this game as a course project.</p>
-                            <p className="portfolio_content">HTML | CSS Flex | Javascript | GitHub</p>    
+                            <p className="portfolio_content">HTML | CSS Flex | Javascript | GitHub | Original Photography | Photoshop/Lightroom |</p>    
                             <a href='https://solitonyc.github.io/puzzle-game/' target="_blank"><img className='portfolio_box_image' alt='' src={portfolioImage03} />  </a>        
                             
                         </div>
@@ -44,7 +45,7 @@ export default class Portfolio extends Component {
                         <div class='portfolio_card'>
                             <h2 className="portfolio_card_title">Freelance Website</h2>        
                             <p className="portfolio_content">A website I developed for a client as part of a bigger re-brand project. I worked closely with their executive team in developing their brand, and collaborated with their sales and content writers for the website.</p>
-                            <p className="portfolio_content">HTML | CSS | Javascript | WordPress</p>
+                            <p className="portfolio_content">HTML | CSS | Javascript | WordPress | Original Photography | Photoshop/Lightroom</p>
                             <a href='http://sequentialtech.com/' target="_blank"><img className='portfolio_box_image' alt='' src={portfolioImage04} /></a>       
                             
                         </div>              
@@ -52,10 +53,18 @@ export default class Portfolio extends Component {
                         <div className='portfolio_card'>
                             <h2 className="portfolio_card_title">API Integration &amp; Retrieve</h2>        
                             <p className="portfolio_content">By utilizing fetch to integrate into Marvel's API, I created a search tool that retrieves Marvel characters from it's catalogue that match submitted character strings.</p>
-                            <p className="portfolio_content">HTML | CSS Flex | Javascript | Node | React | Fetch | Marvel API | Heroku</p>                    
+                            <p className="portfolio_content">HTML | CSS Flex | Javascript | Node | React | Fetch | Marvel API | GitHub | Heroku | Photoshop</p>                    
                             <a href='https://ancient-shelf-14355.herokuapp.com/welcome' target="_blank"> <img className='portfolio_box_image' alt='' src={portfolioImage05} /></a>        
                             
-                        </div>                    
+                        </div> 
+
+                        <div className='portfolio_card'>
+                            <h2 className="portfolio_card_title">React.js App &amp; Message Form</h2>        
+                            <p className="portfolio_content">A portfolio website built entirely as a React.js app. Each section serve as components in this single page app. Message form delivery is handled through Netlify hosting services.</p>
+                            <p className="portfolio_content">CSS Flex | Javascript | Node | React.js | Netlify | Google Fonts | Photoshop/Lightroom</p>                    
+                            <a href='https://github.com/solitonyc/solito-portfolio' target="_blank"> <img className='portfolio_box_image' alt='' src={portfolioImage06} /></a>        
+                            
+                        </div>                        
             
             </div>
         )
